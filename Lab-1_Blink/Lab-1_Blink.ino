4ecaad46427b6c103e67c4eb42baa1fc97f6acd7
Luke Marchand <02luke.marchand@gulllakecs.org>
2019-11-14 14:15:23 -0500

    Add Beginning Program

--- Lab-1_Blink/Lab-1_Blink.ino
+++ Lab-1_Blink/Lab-1_Blink.ino
@@ -6,14 +6,22 @@
    turn it off, and back on in a While Loop
 */
 
-
+//digital pin2 has a push button attached to it
+int pushButton = 2;
 
 void setup() {
-  
+  //initializes serial communication at 9600 bits per second
+  Serial.begin(9600);
+  //make the pushButton pin an input
+  pinMode(pushButton, INPUT);
   
 }
 
 void loop() {
-  
+  //read the input pin
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button
+  Serial.println(buttonState);
+  delay(250); //delay in between readings for stability
   
 }
