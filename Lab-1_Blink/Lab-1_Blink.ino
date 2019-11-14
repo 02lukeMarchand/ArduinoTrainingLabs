d2315fea9e8a52e2528406c0201a3ae0c489485a
Luke Marchand <02luke.marchand@gulllakecs.org>
2019-11-14 13:41:31 -0500

    Adjust LED Timings

--- Lab-1_Blink/Lab-1_Blink.ino
+++ Lab-1_Blink/Lab-1_Blink.ino
@@ -1,10 +1,10 @@
 /* Programmer: Luke
- * Date: 11.6.19
- * Program: Blink
- * 
- * This Program will turn on an LED for a specified amount of time,
- * turn it off, and back on in a While Loop
- */
+   Date: 11.6.19
+   Program: Blink
+
+   This Program will turn on an LED for a specified amount of time,
+   turn it off, and back on in a While Loop
+*/
 
 // Initialize our led to Pin 13
 int led = 13;
@@ -12,14 +12,14 @@ int led = 13;
 void setup() {
   // Using the Function pinMode to call the variable led and provide Output
   pinMode(led, OUTPUT);
-  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(led, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(led, LOW);
-  delay(1000);
-  
+  delay(2000);
+
 }
